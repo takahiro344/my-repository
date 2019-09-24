8cc2a6ce82b30bada1f76405b4de46c37c8f9f5e
Takahiro Miyoshi <hamuhamu334@yahoo.co.jp>
2019-09-24 21:57:07 +0900

    Refactoring.

--- Kabutan/FiscalPeriodNews/main.py
+++ Kabutan/FiscalPeriodNews/main.py
@@ -8,10 +8,7 @@ import ssl
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-latestNewsFile = "LatestNews.txt"
-
 topURL = "https://kabutan.jp"
-pageURL = topURL + "/news/?&page="
 
 def main():
     tw = Twitter(
@@ -23,39 +20,49 @@ def main():
         )
     )
     
-    latestNews = ""
-    if os.path.exists(latestNewsFile):
-      with open(latestNewsFile, "r", encoding="utf-8") as infile:
-        latestNews = infile.read()
-
-    output = False
-    if not latestNews:
-        output = True
+    latestNews = getSavedLatestNews()
+    output = True if not latestNews else False
 
     for pageNo in reversed(range(1, 22)):
-      eachPageURL = pageURL + str(pageNo)
-      html = urlopen(eachPageURL)
-      bsObj = BeautifulSoup(html, "html.parser")
+        html = openTargetURL(pageNo)
+        bsObj = BeautifulSoup(html, "html.parser")
 
-      table = bsObj.findAll("table", {"class":"s_news_list"})[0]
-      rowList = table.findAll("tr")
+        table = bsObj.findAll("table", {"class":"s_news_list"})[0]
+        rowList = table.findAll("tr")
 
-      for row in reversed(rowList):
-          cols = row.find_all('td')
-          # cols[2]: <td><a href="/news/?b=...">...</a></td>
-          href = cols[2].find('a').get('href')
-          newsURL = topURL + href
+        for row in reversed(rowList):
+            cols = row.find_all('td')
+            # cols[2]: <td><a href="/news/?b=...">...</a></td>
+            href = cols[2].find('a').get('href')
+            newsURL = topURL + href
+            
+            if output:
+                msg = cols[2].text + "\n" + newsURL
+                tw.statuses.update(status=msg)
           
-          if output:
-              msg = cols[2].text + "\n" + newsURL
-              tw.statuses.update(status=msg)
-          
-          if not output and cols[2].text in latestNews:
-              output = True
-              continue
+            if not output and cols[2].text in latestNews:
+                output = True
+                continue
     
     with open(latestNewsFile, "w", encoding="utf-8") as outfile:
-      outfile.write(cols[2].text)
+        outfile.write(cols[2].text)
+
+latestNewsFile = "LatestNews.txt"
+
+def getSavedLatestNews():
+    latestNews = ""
+
+    if os.path.exists(latestNewsFile):
+        with open(latestNewsFile, "r", encoding="utf-8") as infile:
+            latestNews = infile.read()
+    
+    return latestNews
+
+pageURL = topURL + "/news/?&page="
+
+def openTargetURL(pageNo):
+    targetURL = pageURL + str(pageNo)
+    return urlopen(targetURL)
 
 if __name__ == "__main__":
     main()
