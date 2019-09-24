6823db02833d8e6c4cea57e665c66321bd18940b
Takahiro Miyoshi <hamuhamu334@yahoo.co.jp>
2019-09-24 21:38:17 +0900

    Correct processing in iterating table elements.

--- Kabutan/FiscalPeriodNews/main.py
+++ Kabutan/FiscalPeriodNews/main.py
@@ -19,7 +19,7 @@ def main():
             config.TW_TOKEN,
             config.TW_TOKEN_SECRET,
             config.TW_CONSUMER_KEY,
-            config.TW_CONSUMER_SECRET,
+            config.TW_CONSUMER_SECRET
         )
     )
     
@@ -45,14 +45,14 @@ def main():
           # cols[2]: <td><a href="/news/?b=...">...</a></td>
           href = cols[2].find('a').get('href')
           newsURL = topURL + href
-
-          if output == False and latestNews == cols[2].text:
-              output = True
-              continue
-
-          if output == True:
+          
+          if output:
               msg = cols[2].text + "\n" + newsURL
               tw.statuses.update(status=msg)
+          
+          if not output and cols[2].text in latestNews:
+              output = True
+              continue
     
     with open(latestNewsFile, "w", encoding="utf-8") as outfile:
       outfile.write(cols[2].text)
